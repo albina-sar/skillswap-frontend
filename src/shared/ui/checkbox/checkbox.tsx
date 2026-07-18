5a01cdbcc960538026f78ec139fbcf1a6b62db10
Yuliana Galkina <yayltw17@yandex.ru>
2026-07-18 18:30:47 +0700

    fix: состояние checked перенесено в пропсы

--- src/shared/ui/checkbox/checkbox.tsx
+++ src/shared/ui/checkbox/checkbox.tsx
@@ -1,14 +1,11 @@
 import styles from './checkbox.module.css';
 import { checkboxProps } from './types';
 import clsx from 'clsx';
-import { useState } from 'react';
 
-export const Checkbox = ({ icon, onChange, label, value, isDisabled = false }: checkboxProps) => {
-  const [isChecked, setIsChecked] = useState(false);
+export const Checkbox = ({ icon, onChange, label, value, isChecked, isDisabled = false }: checkboxProps) => {
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setIsChecked(event.target.checked);
-    onChange(event.target.value, event.target.checked);
+    onChange(event.target.value);
   };
 
   return (
