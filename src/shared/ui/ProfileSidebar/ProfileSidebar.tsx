f3207727ac917d02e6f462d742c26af1f1e32f45
Natalya Eremeeva <natasha.yashina.15@gmail.com>
2026-07-18 16:38:52 +0300

    Внесены изменения для навигации между кнопками

--- src/shared/ui/ProfileSidebar/ProfileSidebar.tsx
+++ src/shared/ui/ProfileSidebar/ProfileSidebar.tsx
@@ -1,40 +1,41 @@
-import React, { useState } from 'react'
+import React from 'react'
 import styles from './ProfileSidebar.module.css'
 import requestIcon from '../../assets/icons-sidebar-profile/request.svg';
 import messageTextIcon from '../../assets/icons-sidebar-profile/message-text.svg';
 import likeIcon from '../../assets/icons-sidebar-profile/like.svg';
 import ideaIcon from '../../assets/icons-sidebar-profile/idea.svg';
 import userIcon from '../../assets/icons-sidebar-profile/user.svg';
+import { useLocation } from 'react-router-dom';
+import { Link } from "react-router-dom";
+
 
 const sections = [
-  { id: 'request', label: 'Заявки', icon: requestIcon},
-  { id: 'exchanges', label: 'Мои обмены', icon: messageTextIcon},
-  { id: 'favorites', label: 'Избранное', icon: likeIcon},
-  { id: 'skills', label: 'Мои навыки', icon: ideaIcon},
-  { id: 'profile', label: 'Личные данные', icon: userIcon},
+  { id: 'request', label: 'Заявки', icon: requestIcon, path: '/request'},
+  { id: 'exchanges', label: 'Мои обмены', icon: messageTextIcon, path: '/exchanges'},
+  { id: 'favorites', label: 'Избранное', icon: likeIcon, path: '/favorites'},
+  { id: 'skills', label: 'Мои навыки', icon: ideaIcon, path: '/skills'},
+  { id: 'profile', label: 'Личные данные', icon: userIcon, path: '/profile'},
 ];
 
 export const ProfileSidebar: React.FC = () => {
-  const [activeSection, setActiveSection] = useState<string>('profile');
-
-  const handleClick = (id: string) => {
-    setActiveSection(id);
-  };
+  const location = useLocation();
 
   return (
     <div className={styles.container}>
       <div className={styles.sidebar}>
         {sections.map(section => (
-          <div
-            key={section.id}
-            className={`${styles.item} ${
-              activeSection === section.id ? styles.active : ''
-            }`}
-            onClick={() => handleClick(section.id)}
-          >
-            <img src={section.icon} alt="иконка" />
-            <div>{section.label}</div>
-          </div>
+          <nav>
+            <Link
+              to={section.path}
+              key={section.id}
+              className={`${styles.item} ${
+                location.pathname === section.path ? styles.active : ''
+              }`}
+            >
+              <img src={section.icon} alt={section.id} />
+              <div>{section.label}</div>
+            </Link>
+          </nav>
         ))}
       </div>
     </div>
