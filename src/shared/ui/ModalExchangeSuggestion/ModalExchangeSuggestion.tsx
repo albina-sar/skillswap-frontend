ca829aaa594ede923e8ae187308b153751a4b65b
Natalya Eremeeva <natasha.yashina.15@gmail.com>
2026-07-20 13:20:14 +0300

    Исправлены ошибки

--- src/shared/ui/ModalExchangeSuggestion/ModalExchangeSuggestion.tsx
+++ src/shared/ui/ModalExchangeSuggestion/ModalExchangeSuggestion.tsx
@@ -15,8 +15,8 @@ export function ModalExchangeSuggestion() {
       <Modal isOpen={isModalOpen} onClose={closeModal}>
         <img src={notificationIcon} alt="Иконка уведомления" />
         <div className={styles.textContent}>
-          <h2 className={styles.title}>Вы предложили обмен</h2>
-          <p className={styles.textParagraph}>Теперь дождитесь подтверждения. Вам придёт уведомление</p>
+          <h2 className={styles.title}>Вы предложили обмен</h2>
+          <p className={styles.textParagraph}>Теперь дождитесь подтверждения. Вам придёт уведомление</p>
         </div>
         <Button variant="primary" size="large" onClick={closeModal}>Готово</Button>
       </Modal>
