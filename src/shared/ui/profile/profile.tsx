e594d2989592109141975659216b2f561674ad44
Whyno <79536860663@mail.ru>
2026-07-20 20:11:31 +0300

    feat: add profile user menu

--- src/shared/ui/profile/profile.tsx
+++ src/shared/ui/profile/profile.tsx
@@ -1,21 +1,42 @@
-import { ProfileUIProps } from "./types";
-import styles from './profile.module.css'
-import { Avatar } from "@/components/avatar-element";
-import { FC, memo } from "react";
+import { FC, memo, useState } from 'react';
 
-export const ProfileUIComponent: FC<ProfileUIProps> = ({image, name}) => {
+import { Avatar } from '@/components/avatar-element';
+
+import { UserMenu } from './UserMenu';
+import { ProfileUIProps } from './types';
+import styles from './profile.module.css';
+
+export const ProfileUIComponent: FC<ProfileUIProps> = ({
+  image,
+  name,
+}) => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const handleClick = () => {
-    console.log('заглушка для отображения popup menu');
-  }
-  return(
-    <button className={styles.container} onClick={handleClick}>
-      <span className={styles.text}>{name}</span>
-      <Avatar image={image} name={name} size='xs' />
-    </button>
-  )
-}
+    setIsMenuOpen((prev) => !prev);
+  };
+
+  return (
+    <div className={styles.wrapper}>
+      <button
+        className={styles.container}
+        onClick={handleClick}
+        type="button"
+      >
+        <span className={styles.text}>{name}</span>
+
+        <Avatar
+          image={image}
+          name={name}
+          size="xs"
+        />
+      </button>
+
+      {isMenuOpen && <UserMenu />}
+    </div>
+  );
+};
 
 ProfileUIComponent.displayName = 'ProfileUI';
 
-export const ProfileUI = memo(ProfileUIComponent)
+export const ProfileUI = memo(ProfileUIComponent);
