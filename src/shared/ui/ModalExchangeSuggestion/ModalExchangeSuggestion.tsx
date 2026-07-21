f51eec9f3118627a7a8115d7303755290fc8b6f4
Natalya Eremeeva <natasha.yashina.15@gmail.com>
2026-07-21 23:01:02 +0300

    Внесены правки

--- src/shared/ui/ModalExchangeSuggestion/ModalExchangeSuggestion.tsx
+++ src/shared/ui/ModalExchangeSuggestion/ModalExchangeSuggestion.tsx
@@ -1,25 +1,35 @@
-import { useState } from 'react';
 import { Modal } from '../Modal/Modal';
 import styles from './ModalExchangeSuggestion.module.css'
 import notificationIcon from '../../assets/icons/notification.svg';
 import { Button } from '../button/button';
 
-export function ModalExchangeSuggestion() {
-  const [isModalOpen, setModalOpen] = useState(false);
+interface Props {
+  isModalOpened: boolean;
+  onCloseModal: () => void;
+}
 
-  const openModal = () => setModalOpen(true);
-  const closeModal = () => setModalOpen(false);
+export function ModalExchangeSuggestion(props: Props) {
   return (
     <div>
-      <Button onClick={openModal} variant="primary">Открыть модальное окно</Button>
-      <Modal isOpen={isModalOpen} onClose={closeModal}>
+      <Modal isOpen={props.isModalOpened} onClose={props.onCloseModal}>
         <img src={notificationIcon} alt="Иконка уведомления" />
         <div className={styles.textContent}>
           <h2 className={styles.title}>Вы предложили обмен</h2>
           <p className={styles.textParagraph}>Теперь дождитесь подтверждения. Вам придёт уведомление</p>
         </div>
-        <Button variant="primary" size="large" onClick={closeModal}>Готово</Button>
+        <Button variant="primary" size="large" onClick={props.onCloseModal}>Готово</Button>
       </Modal>
     </div>
   );
 };
+
+// Пример использования
+// export default function ExchangeSuggestion() {
+//     const [isModalOpened, setIsModalOpened] = useState(false);
+//   return (
+//     <main>
+//        <ModalExchangeSuggestion isModalOpened={isModalOpened} onCloseModal={() => setIsModalOpened(false)}/>
+//       <Button onClick={() => setIsModalOpened(true)}>Открыть</Button>
+//     </main>
+//   )
+// }
