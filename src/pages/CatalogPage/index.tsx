76c42e579e77cc0cb59501218a7ff16106098ce9
Natalya Eremeeva <natasha.yashina.15@gmail.com>
2026-07-22 19:07:11 +0300

    Собран usersSlice, подключен reducer, данные выведены в CatalogPage

--- src/pages/CatalogPage/index.tsx
+++ src/pages/CatalogPage/index.tsx
@@ -1,10 +1,28 @@
 // TODO: реализовать страницу CatalogPage
+import { useEffect } from 'react';
+import { useAppDispatch, useAppSelector } from '../../store/hooks';
+import { fetchUsersThunk } from '../../store/usersSlice';
 
 export default function CatalogPage() {
+  const dispatch = useAppDispatch();
+  const { users } = useAppSelector((state) => state.users);
+
+  useEffect(() => {
+    dispatch(fetchUsersThunk());
+  }, [dispatch]);
+
   return (
     <main>
       <h1>CatalogPage</h1>
       <p>Страница в разработке</p>
+      <div>
+        <h2>Список пользователей</h2>
+        <ul>
+          {users.map((user) => (
+            <li key={user.id}>{user.name}</li>
+          ))}
+        </ul>
+      </div>
     </main>
   )
 }
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
+import usersReducer from './usersSlice';
 // Импортируй свои slice'ы здесь по мере их создания:
 // import skillsReducer from '@/entities/skill/model/skillsSlice'
 // import authReducer from '@/features/auth/model/authSlice'
 
 export const store = configureStore({
   reducer: {
+    users: usersReducer,
     // skills: skillsReducer,
     // auth: authReducer,
   },
