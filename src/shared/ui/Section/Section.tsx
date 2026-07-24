502cd6cdd67ec2b211c745ea99bb6730fdb3eef3
Kirill Filipchuk <filipchuckk@yandex.ru>
2026-07-24 13:11:40 +0300

    заголовки секций с кнопкой

---
+++ src/shared/ui/Section/Section.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import clsx from 'clsx'
+import styles from './Section.module.css'
+import type { SectionProps } from './types'
+import { Button } from '../button/button'
+import ArrowIcon from '@/shared/assets/icons/VectorRight.svg?react'
+
+export const Section: React.FC<SectionProps> = ({
+  title,
+  showAllButton = false,
+  onSeeAll,
+  children,
+}) => {
+  return (
+    <section className={clsx(styles.section)}>
+      <div className={styles.header}>
+        <h2 className={styles.title}>{title}</h2>
+        {showAllButton && (
+          <Button
+            variant="outline"
+            onClick={onSeeAll}
+            className={styles.seeAllButton}
+            image={<ArrowIcon />}
+            imagePosition="right"
+          >
+            Смотреть все
+          </Button>
+        )}
+      </div>
+      <div className={styles.content}>{children}</div>
+    </section>
+  )
+}
