221811e092e3f24f3150190a288c9dd4e3c73f7d
egenok94 <62773612+egenok94@users.noreply.github.com>
2026-07-26 23:32:53 +0300

    add userCardElement and some fix in userslice and UserCard css

---
+++ src/components/usercard-element/usercard-elemetn.tsx
@@ -0,0 +1,50 @@
+import { loadSkills, selectSkills } from "@/entities/skill/model/skillsSlice";
+import { fetchUsersThunk, selectUsers } from "@/entities/user/model/usersSlice";
+import { CATEGORIES_DATA } from "@/shared/lib/constants";
+import { Skill, Subcategory, User } from "@/shared/types";
+import { UserCard } from "@/shared/ui/UserCard";
+import { AppDispatch } from "@/store";
+import { FC, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+interface UserCardElementProps {
+  authorid: string;
+}
+
+export const UserCardElement: FC<UserCardElementProps> = ({authorid}) => {
+  const dispatch: AppDispatch = useDispatch();
+  const skills = useSelector(selectSkills) as Skill[];
+  const users = useSelector(selectUsers) as User[];
+
+  if (users.length === 0 || skills.length === 0) {
+    return <div>Загрузка...</div>;
+  }
+
+  // получаем пользователя
+  const getedUser = users.find(user => user.id === authorid) as User;
+
+  if(!getedUser) {
+    return <div>Автор не найден</div>
+  }
+
+  //чему может научить
+  console.log("skills", skills);
+  const getTeachSkill = skills.find(skill => getedUser.skills.includes(skill.id)) as Skill;
+
+  console.log("getTeachSkill", getTeachSkill);
+
+  //чему хочет научиться
+  const getLearnSkills: Subcategory[] = CATEGORIES_DATA
+    .flatMap(category => category.subcategories)
+    .filter(sub => getedUser.wantsToLearn.includes(sub.id));
+
+  console.log("getLearnSkills", getLearnSkills);
+
+  const handleclick = () => {
+    console.log("заглушка для клика по карточке");
+  }
+
+  return (
+    <UserCard user={getedUser} teachSkill={getTeachSkill} learnSkills={getLearnSkills} onDetailsClick={handleclick} />
+  )
+}
