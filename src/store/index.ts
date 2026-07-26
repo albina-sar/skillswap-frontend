2d54e43bbc9e5c3c922eea9d212709605b214e3e
Yuliana Galkina <yayltw17@yandex.ru>
2026-07-26 15:35:01 +0700

    fix: в стор подключен редьюсер аккаунта

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,12 +3,14 @@ import usersReducer from '../entities/user/model/usersSlice';
 // Импортируй свои slice'ы здесь по мере их создания:
 import skillsReducer from '@/entities/skill/model/skillsSlice'
 import authReducer from '@/entities/auth/model/authSlice'
+import accountReducer from '@/entities/account/model/accountSlice';
 
 export const store = configureStore({
   reducer: {
     users: usersReducer,
     skills: skillsReducer,
     auth: authReducer,
+    account: accountReducer,
   },
 })
 
